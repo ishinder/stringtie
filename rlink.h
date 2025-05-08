327f6987b999d4ffced28fb8cff9ad43821e70a5
Ida Shinder <32040472+ishinder@users.noreply.github.com>
2025-05-08 16:35:41 -0400

    Revert "Improve poly(A)/poly(T) artifact detection and transcript assembly (#1)"
    This reverts commit 723c27cb8ae4aab3b75ae17f86634a335b9870e5.

--- rlink.h
+++ rlink.h
@@ -76,13 +76,12 @@ struct CTransfrag {
 	int guide;
 	uint longstart; // for long reads: min start of all longreads sharing transfrag
 	uint longend; // for long reads: max end of all longreads sharing transfrag
-	bool is_artifact:1; // set if this transfrag is an artifact
-	CTransfrag(GVec<int>& _nodes,GBitVec& bit, float abund=0, bool treal=false, int tguide=0,float sr=0):nodes(_nodes),pattern(bit),abundance(abund),srabund(sr),path(),usepath(-1),weak(-1),real(treal),longread(false),shortread(false),guide(tguide),longstart(false),longend(false),is_artifact(false) {}
-	CTransfrag(float abund=0, bool treal=false,int tguide=0):nodes(),pattern(),abundance(abund),srabund(0),path(),usepath(-1),weak(-1),real(treal),longread(false),shortread(false),guide(tguide),longstart(false),longend(false),is_artifact(false) {
+	CTransfrag(GVec<int>& _nodes,GBitVec& bit, float abund=0, bool treal=false, int tguide=0,float sr=0):nodes(_nodes),pattern(bit),abundance(abund),srabund(sr),path(),usepath(-1),weak(-1),real(treal),longread(false),shortread(false),guide(tguide),longstart(false),longend(false) {}
+	CTransfrag(float abund=0, bool treal=false,int tguide=0):nodes(),pattern(),abundance(abund),srabund(0),path(),usepath(-1),weak(-1),real(treal),longread(false),shortread(false),guide(tguide),longstart(false),longend(false) {
 	}
 };
 
-struct CMTransfrag { // this is the super-class for transfrag -> to use in case of merging trans ipts
+struct CMTransfrag { // this is the super-class for transfrag -> to use in case of merging transcripts
 	CTransfrag *transfrag;
 	GVec<int> read; // all reads' indeces that are connected to this transfrag
 	int nf;
